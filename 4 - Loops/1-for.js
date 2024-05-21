1478c7bfe9ce92654d8fb128b06890376a4092c0
KaiqueT <103083508+KaciqueT@users.noreply.github.com>
2024-05-21 15:49:45 -0300

    Delete 4 - Loops directory

--- 4 - Loops/1-for.js
+++
@@ -1,8 +0,0 @@
-for(let i = 1; i < 10; i ++){
-    
-    if(i%2==0){
-        console.log("O valor " + i + " é par")
-    }else{
-        console.log("O valor " + i + " é impar")
-    }
-}
--- 4 - Loops/4 dowhile.js
+++
@@ -1,6 +0,0 @@
-let n=10
-
-do{
-    console.log(n)
-    n++
-}while(n<100)
